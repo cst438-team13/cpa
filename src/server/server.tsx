bb54485125f5a64a1e129b7e400c450398d3b373
Elijah <elitkramer@gmail.com>
2024-02-21 19:03:14 -0800

    Use antd

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -11,8 +11,8 @@ app.get("/", (_req, res) => {
   const html = ReactDOMServer.renderToString(
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="css/app.css" rel="stylesheet" />
         <title>CPA</title>
       </head>
       <body>
