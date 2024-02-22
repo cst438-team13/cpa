2070c37bc3b24bbe8153414ce0af420b0962009a
Elijah <elitkramer@gmail.com>
2024-02-22 15:44:34 -0800

    Extend App.tsx

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -3,8 +3,9 @@ import { Button } from "antd";
 
 export function App() {
   return (
-    <div style={{ padding: 24 }}>
-      <Button>Click me</Button>
+    <div style={{ padding: 24, gap: 8, display: "flex" }}>
+      <Button type="primary">Click me</Button>
+      <Button>Or don't</Button>
     </div>
   );
 }
