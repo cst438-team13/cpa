cadd2925a142d10b0d32d2ecbf3b6c237902b6e1
Elijah <elitkramer@gmail.com>
2024-02-28 15:50:09 -0800

    Setup db and typeorm

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -1,6 +1,8 @@
 import express from "express";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import { User } from "./models/User";
+import { DB } from "./db";
 
 const app = express();
 const port = 3000;
@@ -25,6 +27,24 @@ app.get("*", (_req, res) => {
   res.send(html);
 });
 
-app.listen(port, () => {
-  console.log(`Now running at http://localhost:${port}`);
-});
+function onInitServer() {
+  // Add a user to the db
+  const user: User = new User();
+  user.username = "abc";
+  user.name = "Person";
+
+  DB.save(user);
+}
+
+console.log("Connecting to DB...");
+
+DB.init()
+  .then(() => {
+    console.log("Connected to DB");
+    onInitServer();
+
+    app.listen(port, () => {
+      console.log(`Now running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
