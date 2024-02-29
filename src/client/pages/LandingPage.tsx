b2814e8164ec54a2f9e6f385b602b1e8cb83139e
Elijah <elitkramer@gmail.com>
2024-02-29 14:46:44 -0800

    Add onClick handler to login button

--- src/client/pages/LandingPage.tsx
+++ src/client/pages/LandingPage.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { RegisterButton } from "../components/RegisterButton";
 import styled from "styled-components";
 import { Button, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
         <Typography.Title>CPA Prototype</Typography.Title>
 
-        <Button type="primary">Log in</Button>
+        <Button type="primary" onClick={() => navigate("/login")}>
+          Log in
+        </Button>
         <RegisterButton />
       </div>
     </Container>
