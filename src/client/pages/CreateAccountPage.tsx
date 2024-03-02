5e4b8b04fe0af657a660e7ea94e7debf497bb7c5
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-03-02 11:52:34 -0800

    added form for username and password.

--- src/client/pages/CreateAccountPage.tsx
+++ src/client/pages/CreateAccountPage.tsx
@@ -1,18 +1,62 @@
-import { Button, Typography } from "antd";
-import React from "react";
+import { Button, Form, FormInstance, Input, Typography, message } from "antd";
+import FormItem from "antd/es/form/FormItem";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 
 export function CreateAccountPage() {
   const navigate = useNavigate();
+  // const { loginUser } = useAuth();
+
+  const onSubmit = async (values) => {
+    // TODO: check if username is taken
+    // TODO: Check if Password is a valid format
+    if (values.password != values.confirmPassword) {
+      message.error("Passwords do not match.");
+      return;
+    }
+    // TODO: encrypt passord before sending to server
+    // TODO: create new user with given info
+    console.log(values.username);
+    console.log(values.password);
+  };
+
+  const formRef = useRef<FormInstance>();
+
   return (
     <Container>
-      <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
+      <Form onFinish={onSubmit} autoComplete="off" ref={formRef}>
         <Typography.Title>Create Account</Typography.Title>
-        <Button type="primary" onClick={() => navigate("/")}>
-          Go Back
-        </Button>
-      </div>
+        <div>
+          <FormItem
+            label="Username"
+            name="username"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </FormItem>
+
+          <FormItem
+            label="Password"
+            name="password"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </FormItem>
+
+          <FormItem
+            label="Confirm Password"
+            name="confirmPassword"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </FormItem>
+
+          <Button type="primary" htmlType="submit">
+            Create Account!
+          </Button>
+        </div>
+      </Form>
     </Container>
   );
 }
