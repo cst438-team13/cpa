bffeb7cf3927b3a7cbfcd732c77128f896f60872
Elijah <elitkramer@gmail.com>
2024-03-12 15:51:58 -0700

    Begin work on home page

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,9 +1,12 @@
+import { Loading3QuartersOutlined } from "@ant-design/icons";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ConfigProvider } from "antd";
-import React from "react";
+import { ConfigProvider, Flex, Spin } from "antd";
+import React, { Suspense } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useCurrentUserProfile } from "../hooks/useCurrentUserProfile";
 import { CreateAccountPage } from "./pages/CreateAccountPage";
+import { HomePage } from "./pages/HomePage";
 import { LandingPage } from "./pages/LandingPage";
 import { LoginPage } from "./pages/LoginPage";
 import { ManageAccountPage } from "./pages/ManageAccountPage";
@@ -14,7 +17,10 @@ export function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <LandingPage />,
+      element: React.createElement(() => {
+        const user = useCurrentUserProfile();
+        return user == null ? <LandingPage /> : <HomePage />;
+      }),
     },
     {
       path: "/login",
@@ -34,9 +40,25 @@ export function App() {
     <ConfigProvider>
       <QueryClientProvider client={queryClient}>
         <GoogleOAuthProvider clientId="564916212911-csikdeti37k8tm5uik4kqtc9rgk3a1iv.apps.googleusercontent.com">
-          <RouterProvider router={router} />
+          <Suspense fallback={<SuspenseLoadingSpinner />}>
+            <RouterProvider router={router} />
+          </Suspense>
         </GoogleOAuthProvider>
       </QueryClientProvider>
     </ConfigProvider>
+  );
+}
+
+function SuspenseLoadingSpinner() {
+  return (
+    <Flex
+      style={{ width: "100%", height: "100%" }}
+      align="center"
+      justify="center"
+    >
+      <Spin
+        indicator={<Loading3QuartersOutlined style={{ fontSize: 32 }} spin />}
+      />
+    </Flex>
   );
 }
