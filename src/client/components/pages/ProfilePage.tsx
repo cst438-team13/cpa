d1c8768f5f4eabf6fd487ff940f1287c162f4701
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-03-16 10:28:20 -0700

    linked up profile button to profile page.

--- src/client/components/pages/ProfilePage.tsx
+++ src/client/components/pages/ProfilePage.tsx
@@ -1,16 +1,28 @@
-import { FormInstance } from "antd";
-import React, { useRef } from "react";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Card, Flex } from "antd";
+import React from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { useCurrentUserProfile } from "../../hooks/useCurrentUserProfile";
+import { MainLayout } from "../shared/MainLayout";
 
 export function ProfilePage() {
   const navigate = useNavigate();
   const user = useCurrentUserProfile();
 
-  const formRef = useRef<FormInstance>(null);
+  // const formRef = useRef<FormInstance>(null);
 
-  return <Container></Container>;
+  return (
+    <MainLayout>
+      <Flex vertical align="center" style={{ width: "100%" }}>
+        <Card title="Profile Details" style={{ width: 650 }}>
+          <Flex vertical align="center" style={{ width: "100%" }}>
+            <Avatar size={128} icon={<UserOutlined />}></Avatar>
+          </Flex>
+        </Card>
+      </Flex>
+    </MainLayout>
+  );
 }
 
 const Container = styled("div")`
