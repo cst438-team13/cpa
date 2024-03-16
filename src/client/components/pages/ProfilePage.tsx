0c0eebbaf0e09877223cd8fe054c497591c93dbc
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-03-16 10:58:55 -0700

    basic profile page setup done.

--- src/client/components/pages/ProfilePage.tsx
+++ src/client/components/pages/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Card, Flex } from "antd";
+import { Avatar, Card, Flex, List, Typography } from "antd";
 import React from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
@@ -9,6 +9,8 @@ import { MainLayout } from "../shared/MainLayout";
 export function ProfilePage() {
   const navigate = useNavigate();
   const user = useCurrentUserProfile();
+  const infoTemplates = ["Name:", "Location:", "Language:"];
+  const userInfo = [user?.displayName, user?.location, user?.language];
 
   // const formRef = useRef<FormInstance>(null);
 
@@ -18,6 +20,19 @@ export function ProfilePage() {
         <Card title="Profile Details" style={{ width: 650 }}>
           <Flex vertical align="center" style={{ width: "100%" }}>
             <Avatar size={128} icon={<UserOutlined />}></Avatar>
+            <br></br>
+            <List
+              size="large"
+              dataSource={userInfo}
+              renderItem={(item, index) => (
+                <List.Item>
+                  <Typography.Text strong>
+                    {infoTemplates[index]}
+                  </Typography.Text>{" "}
+                  {item}
+                </List.Item>
+              )}
+            ></List>
           </Flex>
         </Card>
       </Flex>
