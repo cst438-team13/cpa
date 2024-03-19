9dfca7abbe8f5ad1fecd5cf8521e49b432be55a9
Elijah <elitkramer@gmail.com>
2024-03-19 14:58:31 -0700

    CreatePetButton styles (#67)

--- src/client/components/shared/CreatePetButton.tsx
+++ src/client/components/shared/CreatePetButton.tsx
@@ -1,21 +1,13 @@
 import { Button } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useCurrentUserProfile } from "../../hooks/useCurrentUserProfile";
-import { useRefetchQuery } from "../../hooks/useQuery";
 
 export function CreatePetButton() {
   const navigate = useNavigate();
-  const user = useCurrentUserProfile();
-  const refetchQuery = useRefetchQuery();
 
   const onClickButton = () => {
     navigate("/createPet");
   };
 
-  return (
-    <Button type={"primary"} shape="circle" onClick={onClickButton}>
-      Create Pet
-    </Button>
-  );
+  return <Button onClick={onClickButton}>Create Pet</Button>;
 }
