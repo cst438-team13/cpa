4731cec0b0b1ddc298d25ddacf039037f201e6ad
Elijah <elitkramer@gmail.com>
2024-04-30 14:19:33 -0700

    Move DB secrets to .env

--- src/server/db.ts
+++ src/server/db.ts
@@ -16,9 +16,9 @@ interface DBManager extends EntityManager {
 
 const dataSource = new DataSource({
   type: "mysql",
-  host: "r4wkv4apxn9btls2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  username: "usuzxuui6mwipij2",
-  password: "xtt1ask6fapd7tqg",
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
   database: "iv7yk8xt5q8bsq9e",
   entities: [UserAccount, UserProfile, PetProfile, Post, PetTransferRequest],
   synchronize: true,
